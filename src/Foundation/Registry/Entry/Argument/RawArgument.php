b8415091a95863a742ef2c6b122dfbe0c9776253
Patryk Baszak <patryk.baszak@gmail.com>
2026-08-03 07:58:35 +0200

    raw argument

---
+++ src/Foundation/Registry/Entry/Argument/RawArgument.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpArchitecture\LazyOperators\Foundation\Registry\Entry\Argument;
+
+use PhpArchitecture\LazyOperators\Foundation\Registry\Entry\ExpressionArgument;
+
+readonly class RawArgument extends ExpressionArgument
+{
+}

a47420676800e3d8a04bdd1ec1adf7c07b8d00cd
Patryk Baszak <patryk.baszak@gmail.com>
2026-08-03 07:33:52 +0200

    namespace change

--- src/Infrastructure/Exception/LazyOperatorsInfrastructureException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-namespace PhpArchitecture\LazyOperators\Infrastructure\Exception;
-
-use PhpArchitecture\LazyOperators\Foundation\Exception\LazyOperatorsException;
-
-interface LazyOperatorsInfrastructureException extends LazyOperatorsException {}
